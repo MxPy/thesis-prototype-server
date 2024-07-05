fac2080c2f331b5d356fb43198e82e68021e0297
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-07-05 19:18:31 +0200

    add CRUD enpoints

--- routers/autorizationAndAuthentication.py
+++ routers/autorizationAndAuthentication.py
@@ -6,11 +6,11 @@ from typing import List
 from security.hashing import Hasher
 
 router = APIRouter(
-    prefix='/LaR',
-    tags=['LaR'])
+    prefix='/user',
+    tags=['user'])
 
 
-#Post methods
+
 @router.post('/post', status_code=status.HTTP_201_CREATED)
 async def create(request:schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(username = request.username, email = request.email, password = Hasher.get_password_hash(request.password))
@@ -18,6 +18,55 @@ async def create(request:schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.delete('/delete/all', status_code=status.HTTP_204_NO_CONTENT)
+async def delete(db: Session = Depends(get_db)):
+    user = db.query(models.User).all()
+    for us in user:
+        db.delete(us)
+    db.commit()
+    return None
+
+@router.delete('/delete/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {user_id} dosen't exist")
+    db.delete(user)
+    db.commit()
+    return None
+
+@router.get('/get/all', response_model=List[schemas.User])
+async def get(db: Session = Depends(get_db)):
+    user = db.query(models.User).all()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'There are no existing users')
+    return user
+
+@router.get('/get/{user_id}', response_model=schemas.User)
+async def get(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {user_id} dosen't exist")
+    return user
+
+
+@router.put('/{user_id}', response_model=schemas.User)
+async def update(user_id: int, request: schemas.User, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {user_id} dosen't exist")
+    
+    user.username = request.username if request.username else user.username
+    user.email = request.email if request.email else user.email
+    if request.password:
+        user.password = Hasher.get_password_hash(request.password)
+    
+    db.commit()
+    db.refresh(user)
+    return user
+
 
 async def check_user(data: schemas.UserLogin, db):
     user = db.query(models.User).filter(models.User.username == data.username).first()
