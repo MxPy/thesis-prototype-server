9f0325f8ed67cbd3150d8335cef3aed6cc54c61d
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-07-06 18:26:51 +0200

    add debug endpoints

--- routers/appdevelopmnet.py
+++ routers/appdevelopmnet.py
@@ -5,6 +5,7 @@ from database import get_sql_db, get_no_sql_db
 from sqlalchemy.orm import Session
 from typing import List
 from bson import ObjectId
+from pymongo.results import DeleteResult
 
 router = APIRouter(
     prefix='/dev',
@@ -33,6 +34,22 @@ async def get(user_id: int, db: Session = Depends(get_sql_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {user_id} dosen't exist")
     return user
 
+@router.get(
+    "/session/all",
+    response_description="Get a all sessions",
+    response_model=List[models.Session],
+    response_model_by_alias=False,
+)
+async def show_session(db: Session = Depends(get_no_sql_db)):
+    """
+    Get all records of sessions.
+    """
+    if (
+        students :=await db.find().to_list(1000)
+    ) is not None:
+        return students
+
+    raise HTTPException(status_code=404, detail=f"any sessions found")
 
 @router.get(
     "/session/{id}",
@@ -40,16 +57,16 @@ async def get(user_id: int, db: Session = Depends(get_sql_db)):
     response_model=models.Session,
     response_model_by_alias=False,
 )
-async def show_student(id: str, db: Session = Depends(get_no_sql_db)):
+async def show_session(id: str, db: Session = Depends(get_no_sql_db)):
     """
-    Get the record for a specific student, looked up by `id`.
+    Get the record for a specific session, looked up by `id`.
     """
     if (
         student := await db.find_one({"_id": ObjectId(id)})
     ) is not None:
         return student
 
-    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+    raise HTTPException(status_code=404, detail=f"session {id} not found")
 
 @router.get(
     "/session/session_id/{id}",
@@ -57,21 +74,33 @@ async def show_student(id: str, db: Session = Depends(get_no_sql_db)):
     response_model=models.Session,
     response_model_by_alias=False,
 )
-async def show_student(id: str, db: Session = Depends(get_no_sql_db)):
+async def show_session(id: str, db: Session = Depends(get_no_sql_db)):
     """
-    Get the record for a specific student, looked up by `id`.
+    Get the record for a specific session, looked up by `id`.
     """
     if (
-        student := await db.find_one({"session_id": id})
+        session := await db.find_one({"session_id": id})
     ) is not None:
-        return student
+        return session
 
-    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+    raise HTTPException(status_code=404, detail=f"session {id} not found")
 
-@router.delete("/session/{id}", response_description="Delete a student")
-async def delete_student(id: str, db: Session = Depends(get_no_sql_db)):
+@router.delete("/session/all", response_description="Delete a session")
+async def delete_session(db: Session = Depends(get_no_sql_db)):
     """
-    Remove a single student record from the database.
+    Remove all session records from the database.
+    """
+    delete_result: DeleteResult = await db.delete_many({})
+
+    if delete_result.deleted_count > 0:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail="No sessions found")
+
+@router.delete("/session/{id}", response_description="Delete a session")
+async def delete_session(id: str, db: Session = Depends(get_no_sql_db)):
+    """
+    Remove a single session record from the database.
     """
     delete_result = await db.delete_one({"_id": ObjectId(id)})
 
@@ -80,16 +109,18 @@ async def delete_student(id: str, db: Session = Depends(get_no_sql_db)):
 
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
 
+
+
 @router.post(
     "/session/",
-    response_description="Add new student",
+    response_description="Add new session",
     response_model=models.Session,
     status_code=status.HTTP_201_CREATED,
     response_model_by_alias=False,
 )
-async def create_student(student: models.Session = Body(...), db: Session = Depends(get_no_sql_db)):
+async def create_session(student: models.Session = Body(...), db: Session = Depends(get_no_sql_db)):
     """
-    Insert a new student record.
+    Insert a new session record.
 
     A unique `id` will be created and provided in the response.
     """
